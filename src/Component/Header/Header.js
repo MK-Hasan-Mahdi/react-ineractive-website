1423a0392acf43217024adf94d1844761dd54836
mk-hasan <mkhasan6891@gmail.com>
2022-03-25 21:21:26 +0600

    shop and header component created

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Double You</h1>
+            <h4>Tech Solution in a smart way</h4>
+        </div>
+    );
+};
+
+export default Header;
